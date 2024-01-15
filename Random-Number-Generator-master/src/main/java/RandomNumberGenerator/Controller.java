17c388684c0dafdb15506a2a6ce163c47bf6c26f
eminegurra <153447195+eminegurra@users.noreply.github.com>
2024-01-15 21:03:02 +0100

    Update Controller.java

--- Random-Number-Generator-master/src/main/java/RandomNumberGenerator/Controller.java
+++ Random-Number-Generator-master/src/main/java/RandomNumberGenerator/Controller.java
@@ -1,20 +1,35 @@
 package RandomNumberGenerator;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 @RestController
 public class Controller {
 
-	@RequestMapping("/")
-	public int index() {
-//		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
-//		return "<!doctypehtml><html><head><title>RandomNumber</title></head><body style=\"background:black\"><h1 style="
-//				+ style + ">" + new Random().nextInt(1000001) + "</h1></body></html>";
+    private static final Logger logger = Logger.getLogger(Controller.class.getName());
 
-		return new Random().nextInt(1000001);
-	}
+    public Controller() {
+        try {
+            // Configure logging to append to a file
+            FileHandler fileHandler = new FileHandler("myapp.log", true);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
+            logger.severe("Error configuring logging: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/generate")
+    public ResponseEntity<Integer> generateRandomNumber() {
+        logger.info("Generating random number...");
+        int randomNumber = ThreadLocalRandom.current().nextInt(1, 101);
+        logger.info("Generated random number: " + randomNumber);
+        return ResponseEntity.ok(randomNumber);
+    }
 }
